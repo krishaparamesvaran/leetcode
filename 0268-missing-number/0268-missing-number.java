35810c89cc011246975cf527da9124e219ff4e07
KRISHA A P <krishaparameshvaran1609@gmail.com>
2026-08-22 11:12:22 +0530

    Time: 0 ms (100%), Space: 47.3 MB (49.27%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+       int n=nums.length;
+       int sumOfn=(n*(n+1))/2;
+       int sumOfNums=0;
+       
+       for(int i=0;i<nums.length;i++){
+          sumOfNums+=nums[i];
+       }
+       return sumOfn-sumOfNums;
+        
+    }
+}
